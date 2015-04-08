c0752b3538c79aa7f2deb47224666717b98cccf4
Christopher Evans <chris.evans@us.usana.com>
2015-04-08 12:12:24 -0600

    Initial Commit

---
+++ Fast RSS Reader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fast RSS Reader
+//
+//  Created by chrise26 on 4/8/15.
+//  Copyright (c) 2015 Usana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fast RSS ReaderTests/Fast_RSS_ReaderTests.m
@@ -0,0 +1,40 @@
+//
+//  Fast_RSS_ReaderTests.m
+//  Fast RSS ReaderTests
+//
+//  Created by chrise26 on 4/8/15.
+//  Copyright (c) 2015 Usana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Fast_RSS_ReaderTests : XCTestCase
+
+@end
+
+@implementation Fast_RSS_ReaderTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
